3cf5837b29a17a72a0878cfa1dc99d22728107e8
Hoang-V-Hung <164860937+Hoang-V-Hung@users.noreply.github.com>
2025-05-14 22:31:57 +0700

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,10 +12,10 @@ const About = () => {
             <div className='my-10 flex flex-col md:flex-row gap-12'>
                 <img className='w-full md:max-w-[360px]' src={assets.about_image} alt='' />
                 <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
-                    <p>Chào mừng đến với Prescripto, đối tác đáng tin cậy của bạn trong việc quản lý nhu cầu chăm sóc sức khỏe của bạn một cách thuận tiện và hiệu quả. Tại Prescripto, chúng tôi hiểu những thách thức mà mọi người phải đối mặt khi lên lịch hẹn khám bác sĩ và quản lý hồ sơ sức khỏe của họ.</p>
-                    <p>Prescripto cam kết mang đến sự xuất sắc trong công nghệ chăm sóc sức khỏe. Chúng tôi liên tục nỗ lực cải thiện nền tảng của mình, tích hợp những tiến bộ mới nhất để cải thiện trải nghiệm của người dùng và cung cấp dịch vụ vượt trội. Cho dù bạn đang đặt lịch hẹn đầu tiên hay quản lý việc chăm sóc liên tục, Prescripto luôn ở đây để hỗ trợ bạn trong từng bước.</p>
+                    <p>Chào mừng đến với BookingDoctor, đối tác đáng tin cậy của bạn trong việc quản lý nhu cầu chăm sóc sức khỏe của bạn một cách thuận tiện và hiệu quả. Tại BookingDoctor, chúng tôi hiểu những thách thức mà mọi người phải đối mặt khi lên lịch hẹn khám bác sĩ và quản lý hồ sơ sức khỏe của họ.</p>
+                    <p>BookingDoctor cam kết mang đến sự xuất sắc trong công nghệ chăm sóc sức khỏe. Chúng tôi liên tục nỗ lực cải thiện nền tảng của mình, tích hợp những tiến bộ mới nhất để cải thiện trải nghiệm của người dùng và cung cấp dịch vụ vượt trội. Cho dù bạn đang đặt lịch hẹn đầu tiên hay quản lý việc chăm sóc liên tục, BookingDoctor luôn ở đây để hỗ trợ bạn trong từng bước.</p>
                     <b className='text-gray-800'>Tầm nhìn</b>
-                    <p>Tầm nhìn của chúng tôi tại Prescripto là tạo ra trải nghiệm chăm sóc sức khỏe liền mạch cho mọi người dùng. Chúng tôi hướng đến mục tiêu thu hẹp khoảng cách giữa bệnh nhân và nhà cung cấp dịch vụ chăm sóc sức khỏe, giúp bạn dễ dàng tiếp cận dịch vụ chăm sóc bạn cần, khi bạn cần.</p>
+                    <p>Tầm nhìn của chúng tôi tại BookingDoctor là tạo ra trải nghiệm chăm sóc sức khỏe liền mạch cho mọi người dùng. Chúng tôi hướng đến mục tiêu thu hẹp khoảng cách giữa bệnh nhân và nhà cung cấp dịch vụ chăm sóc sức khỏe, giúp bạn dễ dàng tiếp cận dịch vụ chăm sóc bạn cần, khi bạn cần.</p>
                 </div>
             </div>
 
